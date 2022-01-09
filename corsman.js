715ac7466dc6d7870eb68ffcb3b2c5f967b94626
vencax <vencax@centrum.cz>
2022-01-09 14:11:05 +0100

    corsman fix

--- corsman.js
+++ corsman.js
@@ -8,10 +8,12 @@ let CONFIGS = []
 
 // taken from https://github.com/expressjs/cors#configuring-cors-asynchronously
 function configCallback (req, callback) {
-  const ALLOWED = CONFIGS[req.params.tenantid]
-  const origin = (ALLOWED && ALLOWED.cors !== undefined) ? ALLOWED.cors : false
+  const CONF = CONFIGS[req.params.tenantid]
+  const allowed = CONF && 
+    CONF.cors !== undefined && 
+    CONF.cors.indexOf(req.headers['origin']) >= 0
   // callback expects two parameters: error and options
-  callback(null, { origin })
+  callback(null, { origin: allowed })
 }
 
 export const corsMW = process.env.NODE_ENV === 'test'
